1ace1e5735730516617ecfd7c8cd9ed31545db7b
2390438889 <2390438889@qq.com>
2019-05-16 14:38:01 +0800

    pom.xml

--- src/main/java/com/lujie/mvc/controller/TestController.java
+++ src/main/java/com/lujie/mvc/controller/TestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class TestController {
     /**
-     * 首页22
+     * 首页11
      * @return
      */
     @RequestMapping("/index")
